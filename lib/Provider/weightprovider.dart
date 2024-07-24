36487f6d2aa4453a7e384c6db07f325cb25420f3
Nikhithasriram <nikhithasriram.552@gmail.com>
2024-07-25 00:23:35 +0530

    adding newWeights depending on date time it occured

--- lib/Provider/weightprovider.dart
+++ lib/Provider/weightprovider.dart
@@ -7,8 +7,26 @@ class WeightProvider extends ChangeNotifier {
   UnmodifiableListView<NewWeight> get items => UnmodifiableListView(_item);
 
   void add(NewWeight w) {
-    _item.add(w);
-    notifyListeners();
+    if (_item.isEmpty) {
+      _item.add(w);
+      notifyListeners();
+    } else {
+      bool added = false;
+      DateTime weightdate = mydatetime(w);
+      for (int i = 0; i < _item.length; i++) {
+        DateTime idate = mydatetime(_item[i]);
+        if (weightdate.isAfter(idate)) {
+          _item.insert(i, w);
+          notifyListeners();
+          added = true;
+          break;
+        }
+      }
+      if (added == false) {
+        _item.add(w);
+        notifyListeners();
+      }
+    }
   }
 
   void delete(NewWeight w) {
@@ -17,7 +35,29 @@ class WeightProvider extends ChangeNotifier {
   }
 
   void update(int index, NewWeight w) {
-    _item[index] = w;
+    delete(_item[index]);
+    add(w);
     notifyListeners();
   }
+}
+
+DateTime mydatetime(NewWeight w) {
+  String date = w.date.split('-')[0];
+  String month = w.date.split('-')[1];
+  String year = w.date.split('-')[2];
+  date = int.parse(date) < 10 ? "0$date" : date;
+  month = int.parse(month) < 10 ? "0$month" : month;
+  String period = w.time.split(" ")[1];
+  int hrs = int.parse((w.time.split(" ")[0]).split(":")[0]);
+  String min = (w.time.split(" ")[0]).split(":")[1];
+  if (hrs == 12 && period == "am") {
+    hrs = 0;
+  }
+  if (period == "pm") {
+    hrs = hrs + 12;
+  }
+  String withzerohr = "";
+  withzerohr = hrs < 10 ? "0$hrs" : hrs.toString();
+
+  return DateTime.parse("$year-$month-$date $withzerohr:$min:00");
 }
